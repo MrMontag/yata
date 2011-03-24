793015c054939ce04ed112dbafb998833095acb1
James Smith <yata.development@gmail.com>
2011-03-24 02:36:09 +0000

    Make build work on windows
    git-svn-id: file:///home/sarajames/tempcode/svnmod/final@108 de2b25a8-641a-0410-966f-9f163a379164

--- cmake-modules/Findyaml-cpp.cmake
+++ cmake-modules/Findyaml-cpp.cmake
@@ -1,26 +1,35 @@
 set(yaml-cpp_FOUND 0)
 
-set(YAML_CPP_INC_SEARCH_PATH
-    /usr/local/include
-    /usr/include)
+if(UNIX)
+    set(YAML_CPP_INC_SEARCH_PATH
+        /usr/local/include
+        /usr/include)
 
-set(YAML_CPP_LIB_SEARCH_PATH
-    /usr/local/lib
-    /usr/lib)
+    set(YAML_CPP_LIB_SEARCH_PATH
+        /usr/local/lib
+        /usr/lib)
+    find_library(YAML_LIB yaml-cpp ${YAML_CPP_LIB_SEARCH_PATH})
+else(UNIX)
+if(WIN32)
+    set(YAML_CPP_BASE_DIR C:/Program\ Files/YAML_CPP)
+    set(YAML_CPP_INC_SEARCH_PATH ${YAML_CPP_BASE_DIR}/include)
 
-find_path(YAML_INCLUDE_PATH yaml-cpp/yaml.h ${YAML_CPP_INC_SEARCH_PATH})
-find_library(YAML_LIB yaml-cpp ${YAML_CPP_LIB_SEARCH_PATH})
+    set(YAML_CPP_LIB_SEARCH_PATH ${YAML_CPP_BASE_DIR}/lib)
+    find_library(YAML_LIB yaml-cpp ${YAML_CPP_LIB_SEARCH_PATH})
+endif(WIN32)
+endif(UNIX)
+
+find_path(YAML_INCLUDE_PATH NAMES yaml-cpp/yaml.h PATHS ${YAML_CPP_INC_SEARCH_PATH})
 
 if(YAML_INCLUDE_PATH AND YAML_LIB)
     set(yaml-cpp_FOUND 1)
+    message("-- Found yaml-cpp")
 endif()
 
-if(yaml-cpp_FOUND)
-else()
+if(NOT yaml-cpp_FOUND)
     if(yaml-cpp_FIND_REQUIRED)
         message(FATAL_ERROR "yaml-cpp not found")
     else()
         message(WARNING "yaml-cpp not found")
     endif()
 endif()
-    
